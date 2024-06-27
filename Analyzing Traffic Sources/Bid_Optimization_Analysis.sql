0b323ec6fc122947f62ac12204e84be27817617d
Dipankar Modak <77185203+Dipankarmodak@users.noreply.github.com>
2024-06-27 01:45:28 -0400

    Update Bid_Optimization_Analysis.sql

--- Analyzing Traffic Sources/Bid_Optimization_Analysis.sql
+++ Analyzing Traffic Sources/Bid_Optimization_Analysis.sql
@@ -1,16 +1,16 @@
- -- Narrowing down the analysis of sessions to order conversion rate based on device type i.e desktop for mobile for gsearch and nonbrand campaign
- -- Based on analysis we see that desktop has more session to order conversion rate
- -- Bidding more on desktop type for gsearch and nonbrand campaign is the optimal bid technqiue
--- Query to identify the sessions to order conversion based on device type i.e desktop or mobile.
- SELECT 
- w.device_type,
- COUNT(DISTINCT w.website_session_id) as sessions,
-  COUNT(DISTINCT o.order_id) as orders,
-COUNT(DISTINCT o.order_id)/ COUNT(DISTINCT w.website_session_id)
-as session_to_order_conv_rate
- FROM website_sessions w LEFT JOIN orders o
- ON w.website_session_id=o.website_session_id
- WHERE w.created_at < '2012-05-11' AND  w.utm_source='gsearch'
- AND w.utm_campaign='nonbrand'
- GROUP BY w.device_type
-  ;
+-- Query to identify the session to order conversion rate based on device type (desktop or mobile) for gsearch and nonbrand campaign
+
+SELECT 
+    w.device_type,                                -- Type of device (e.g., desktop, mobile)
+    COUNT(DISTINCT w.website_session_id) AS sessions, -- Total number of distinct sessions for each device type
+    COUNT(DISTINCT o.order_id) AS orders,         -- Total number of distinct orders for each device type
+    COUNT(DISTINCT o.order_id) / COUNT(DISTINCT w.website_session_id) AS session_to_order_conv_rate -- Session to order conversion rate
+FROM 
+    website_sessions w
+    LEFT JOIN orders o ON w.website_session_id = o.website_session_id
+WHERE 
+    w.created_at < '2012-05-11'                   -- Filter for sessions created before the specified date
+    AND w.utm_source = 'gsearch'                  -- Filter for sessions from the 'gsearch' source
+    AND w.utm_campaign = 'nonbrand'               -- Filter for sessions from the 'nonbrand' campaign
+GROUP BY 
+    w.device_type;                                -- Group by device type to calculate the metrics for each group
