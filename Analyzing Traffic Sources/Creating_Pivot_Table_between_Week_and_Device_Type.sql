efd2b2a51c09e387b4362806e8f3b048be0f548f
Dipankar Modak <77185203+Dipankarmodak@users.noreply.github.com>
2024-06-27 01:46:05 -0400

    Update Creating_Pivot_Table_between_Week_and_Device_Type.sql

--- Analyzing Traffic Sources/Creating_Pivot_Table_between_Week_and_Device_Type.sql
+++ Analyzing Traffic Sources/Creating_Pivot_Table_between_Week_and_Device_Type.sql
@@ -1,12 +1,16 @@
- 
- -- Creating Pivot Table based on device type sessions after increasing the bid on gsearch
- -- and non brand marketing campaign on the desktop device type after the last analysis on bid optimization.
- SELECT 
- MIN(DATE(w.created_at)) as week_start_date,
-  SUM(CASE WHEN w.device_type='desktop' THEN 1 ELSE 0 END) as dtop_sessions,
-SUM(CASE WHEN w.device_type='mobile' THEN 1 ELSE 0 END) as mobile_sessions
- FROM website_sessions w 
- WHERE w.created_at >'2012-04-15' AND w.created_at <'2012-06-09' AND  w.utm_source='gsearch'
- AND w.utm_campaign='nonbrand'
- GROUP BY WEEK(w.created_at)
-  ;
+-- Creating a Pivot Table to analyze sessions based on device type after increasing the bid on gsearch
+-- and non-brand marketing campaign for desktop device type
+
+SELECT 
+    MIN(DATE(w.created_at)) AS week_start_date,  -- The start date of each week
+    SUM(CASE WHEN w.device_type = 'desktop' THEN 1 ELSE 0 END) AS dtop_sessions, -- Total sessions from desktop devices
+    SUM(CASE WHEN w.device_type = 'mobile' THEN 1 ELSE 0 END) AS mobile_sessions -- Total sessions from mobile devices
+FROM 
+    website_sessions w
+WHERE 
+    w.created_at > '2012-04-15'               -- Filter for sessions created after the specified date
+    AND w.created_at < '2012-06-09'           -- Filter for sessions created before the specified date
+    AND w.utm_source = 'gsearch'              -- Filter for sessions from the 'gsearch' source
+    AND w.utm_campaign = 'nonbrand'           -- Filter for sessions from the 'nonbrand' campaign
+GROUP BY 
+    WEEK(w.created_at);                       -- Group the results by week to aggregate the session counts
